88cca9ccc3525098a8f6eb834cba1c036e26ab24
Kevin Kirchhoff <kkirchhoff01@yahoo.com>
2021-08-13 11:53:17 -0400

    Removed unused copy

--- install-web-api.sh
+++ install-web-api.sh
@@ -2,7 +2,6 @@
 
 WEB_PATH=$(pwd)
 
-cp -r ./webdisplay /usr/local/sbin/rpiwebdisplay
 cp rpi-info-webapi.service /lib/systemd/system/
 sed -i -E "s|WEB_PATH|$WEB_PATH|g" /lib/systemd/system/rpi-info-webapi.service
 systemctl enable rpi-info-webapi.service
